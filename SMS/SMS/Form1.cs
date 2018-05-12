7421f3224416408aa65eef7cb12e93e9b01a23de
rizwanalvi <32450360+rizwanalvi@users.noreply.github.com>
2018-05-12 06:13:46 -0700

    CRUD USING GRIDVIEW PART #1

---
+++ SMS/SMS/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SMS
+{
+    public partial class Form1 : Form
+    {
+        Logindb ldb = null;
+        public Form1()
+        {
+            InitializeComponent();
+            ldb = new Logindb();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = ldb.GetLoginTable();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ldb.AddData(new Login { ID = int.Parse(textBox1.Text), UNAME = textBox2.Text, UPASS = textBox3.Text, ROLEID = int.Parse(textBox4.Text) });
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ldb.DeleteData(int.Parse(textBox1.Text));
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ldb.UpdateData(new Login { ID = int.Parse(textBox1.Text), UNAME = textBox2.Text, UPASS = textBox3.Text, ROLEID = int.Parse(textBox4.Text) });
+        }
+
+      
+    }
+}
---
+++ SMS/SMS/Logindb.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace SMS
+{
+     public  class Logindb
+    {
+        SqlConnection _sqlConn = null;
+        List<Login> _loginTable = null;
+        public Logindb() {
+            _sqlConn = new SqlConnection(@"Data Source=faculty18;Initial Catalog=1705Fdb;Integrated Security=True");
+            _sqlConn.Open();
+        }
+
+        public List<Login> GetLoginTable() {
+            _loginTable = new List<Login>();
+            SqlCommand _cmd = new SqlCommand("SELECT * FROM tLogin", _sqlConn);
+            SqlDataReader dReader= _cmd.ExecuteReader();
+            while (dReader.Read()) {
+
+                _loginTable.Add(new Login {ID=dReader.GetInt32(0),UNAME = dReader.GetString(1),UPASS = dReader.GetString(2),ROLEID= dReader.GetInt32(4) });
+            }
+            return _loginTable;
+        }
+
+
+        public void AddData(Login lg) {
+            SqlCommand _cmd = new SqlCommand("INSERT INTO tLogin (ID,UNAME,UPASS,ROLE_ID) VALUES(@ID,@UNAME,@UPASS,@ROLE_ID)", _sqlConn);
+            _cmd.Parameters.AddWithValue("@ID",lg.ID);
+            _cmd.Parameters.AddWithValue("@UNAME", lg.UNAME);
+            _cmd.Parameters.AddWithValue("@UPASS", lg.UPASS);
+            _cmd.Parameters.AddWithValue("@ROLE_ID", lg.ROLEID);
+            _cmd.ExecuteNonQuery();
+        }
+        public void UpdateData(Login lg)
+        {
+
+            SqlCommand _cmd = new SqlCommand("UPDATE tLogin SET UNAME=@Uuname,UPASS=@upass,ROLE_ID=@rid) WHERE ID=@id", _sqlConn);
+
+            _cmd.Parameters.AddWithValue("@Uuname", lg.UNAME);
+            _cmd.Parameters.AddWithValue("@upass", lg.UPASS);
+            _cmd.Parameters.AddWithValue("@rid", lg.ROLEID);
+            _cmd.ExecuteNonQuery();
+
+
+        }
+
+        public void DeleteData(int Id)
+        {
+            _sqlConn.Close();
+            _sqlConn.Open();
+            SqlCommand _cmd = new SqlCommand("DELETE From tLogin where id = @id", _sqlConn);
+            _cmd.Parameters.AddWithValue("@id", Id);
+            _cmd.ExecuteNonQuery();
+
+        }
+
+
+    }
+}
