ae4de890a6272c1426249d2ea6e5eac9f659cb2a
rizwanalvi <32450360+rizwanalvi@users.noreply.github.com>
2018-05-12 07:15:11 -0700

    Add New Class Student

---
+++ SMS/SMS/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS
+{
+   public class Student
+    {
+        public int ID { get; set; }
+        public String StudentName { get; set; }
+        public String FatherName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public String Address { get; set; }
+        public String Gender { get; set; }
+        public String ContactNo01 { get; set; }
+        public String ContactNo02 { get; set; }
+        public String ContactNo03 { get; set; }
+        public String Course { get; set; }
+        public String CurrentSkill { get; set; }
+        public String BatchNo { get; set; }
+        
+    }
+}
